b21d256e64f89f2cf0349e67acb85473a52ec60e
Zhangzhiming1 <1049196689@qq.com>
2017-07-04 17:23:48 +0800

    Update MainActivity.java

--- app/src/main/java/com/renqi/takemedicine/MainActivity.java
+++ app/src/main/java/com/renqi/takemedicine/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //第二次
+        setContentView(R.layout.activity_main);
     }
 }
