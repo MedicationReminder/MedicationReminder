de0ae85e224625110d88846cd18da3769d4ce54a
Xu Wei <759974029@qq.com>
2017-07-05 14:38:57 +0800

    Merge remote-tracking branch 'origin/xw' into xw
    # Conflicts:
    #	MedicationReminder.iml
    #	app/build.gradle
    #	app/src/main/java/com/renqi/takemedicine/activity/MainActivity.java
    #	app/src/main/java/com/renqi/takemedicine/utils/XUtilHttpRequest.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/renqi/takemedicine/activity/MainActivity.java
+++ app/src/main/java/com/renqi/takemedicine/activity/MainActivity.java
@@ -11,13 +11,21 @@ import com.renqi.takemedicine.base.BaseActivity;
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
+
+/**
+ *吃药提醒
+ */
+
+
 @ContentView(R.layout.activity_main)
 public class MainActivity extends BaseActivity {
     @ViewInject(R.id.text) private TextView textView;
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         x.view().inject(this);
-        setToolBarTitle(AppConstants.ToolBarTitle.addContacts);
+        setToolBarTitle(AppConstants.ToolBarTitle.medicationReminder);
+
     }
 }
