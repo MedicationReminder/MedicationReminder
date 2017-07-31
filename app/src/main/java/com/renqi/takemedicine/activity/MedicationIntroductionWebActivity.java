623e864f491e9758630453bad2a43a46d8712a41
zsj <1049196689@qq.com>
2017-07-31 20:28:08 +0800

    Merge remote-tracking branch 'remotes/origin/xw' into zzm
    # Conflicts:
    #	app/src/main/java/com/renqi/takemedicine/base/BaseActivity.java
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/layout/activity_search.xml

--- app/src/main/java/com/renqi/takemedicine/activity/MedicationIntroductionWebActivity.java
+++ app/src/main/java/com/renqi/takemedicine/activity/MedicationIntroductionWebActivity.java
@@ -1,79 +1,89 @@
 package com.renqi.takemedicine.activity;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
+import android.view.KeyEvent;
+import android.view.ViewGroup;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import com.renqi.takemedicine.R;
-import com.renqi.takemedicine.adapter.MedIntroductionAdapter;
 import com.renqi.takemedicine.app.AppConstants;
 import com.renqi.takemedicine.base.BaseActivity;
-import com.renqi.takemedicine.bean.MedIntroductionAdapterBean;
-
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
-
-import java.util.ArrayList;
-import java.util.List;
+import static android.webkit.WebSettings.LOAD_NO_CACHE;
 
 @ContentView(R.layout.activity_medication_introduction_web)
 public class MedicationIntroductionWebActivity extends BaseActivity {
-    @ViewInject(R.id.medicationIntroductionListView)
-    private ListView medicationIntroductionListView;
-private List<MedIntroductionAdapterBean> stringList;
-    private MedIntroductionAdapter medIntroductionAdapter;
+    @ViewInject(R.id.medicationIntroductionWebView)
+    private WebView medicationIntroductionWebView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setToolBarTitle(AppConstants.ToolBarTitle.medicationIntroduction);
-        stringList=new ArrayList<>();
-
-        MedIntroductionAdapterBean medIntroductionAdapterBean=new MedIntroductionAdapterBean("用药参考",R.mipmap.down);
-        stringList.add(medIntroductionAdapterBean);
-
-        MedIntroductionAdapterBean medIntroductionAdapterBean1=new MedIntroductionAdapterBean("中药堂",R.mipmap.down);
-        stringList.add(medIntroductionAdapterBean1);
-
-        MedIntroductionAdapterBean medIntroductionAdapterBean2=new MedIntroductionAdapterBean("儿童用药",R.mipmap.down);
-        stringList.add(medIntroductionAdapterBean2);
-
-        MedIntroductionAdapterBean medIntroductionAdapterBean3=new MedIntroductionAdapterBean("安全用药",R.mipmap.down);
-        stringList.add(medIntroductionAdapterBean3);
-
-        MedIntroductionAdapterBean medIntroductionAdapterBean4=new MedIntroductionAdapterBean("药师用药",R.mipmap.down);
-        stringList.add(medIntroductionAdapterBean4);
-
-        MedIntroductionAdapterBean medIntroductionAdapterBean5=new MedIntroductionAdapterBean("家庭用药大全",R.mipmap.down);
-        stringList.add(medIntroductionAdapterBean5);
-
-        MedIntroductionAdapterBean medIntroductionAdapterBean6=new MedIntroductionAdapterBean("孕妇用药",R.mipmap.down);
-        stringList.add(medIntroductionAdapterBean6);
-
-        MedIntroductionAdapterBean medIntroductionAdapterBean7=new MedIntroductionAdapterBean("妇科用药",R.mipmap.down);
-        stringList.add(medIntroductionAdapterBean7);
-
-        MedIntroductionAdapterBean medIntroductionAdapterBean8=new MedIntroductionAdapterBean("吃药提醒",R.mipmap.down);
-        stringList.add(medIntroductionAdapterBean8);
-
-
-        medIntroductionAdapter=new MedIntroductionAdapter(MedicationIntroductionWebActivity.this,stringList);
-        medicationIntroductionListView.setAdapter(medIntroductionAdapter);
-        medicationIntroductionListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        initData();
+        medicationIntroductionWebView.loadUrl("http://www.yiliaode.com/strategy_categories/576b48c2391af0550c6798f8/sub_strategy_categories");
+        medicationIntroductionWebView.setWebViewClient(new WebViewClient() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if(stringList.get(position).getId()==R.mipmap.down){
-                    stringList.get(position).setId(R.mipmap.up);
-                }else {
-                    stringList.get(position).setId(R.mipmap.down);
-                }
-                medIntroductionAdapter.notifyDataSetChanged();
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
             }
+
+
         });
+        medicationIntroductionWebView.setWebChromeClient(new WebChromeClient() {
+
+            //配置权限（同样在WebChromeClient中实现）
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin,
+                                                           GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+                super.onGeolocationPermissionsShowPrompt(origin, callback);
+            }
+
+        });
+
     }
 
+    private void initData() {
+        WebSettings settings = medicationIntroductionWebView.getSettings();
+        settings.setJavaScriptEnabled(true);//开启webview支持JS
+        settings.setDatabaseEnabled(true);
+        String dir = this.getApplicationContext().getDir("database", Context.MODE_PRIVATE).getPath();
 
+        //启用地理定位
+        settings.setGeolocationEnabled(true);
+        //设置定位的数据库路径
+        settings.setGeolocationDatabasePath(dir);
+        settings.setDomStorageEnabled(true);
 
+        settings.setCacheMode(LOAD_NO_CACHE);
+    }
 
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && medicationIntroductionWebView.canGoBack()) {
+            medicationIntroductionWebView.goBack();
+            return true;
+        }
 
+        return super.onKeyDown(keyCode, event);
+    }
+
+    //销毁Webview
+    @Override
+    protected void onDestroy() {
+        if (medicationIntroductionWebView != null) {
+            medicationIntroductionWebView.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
+            medicationIntroductionWebView.clearHistory();
+            ((ViewGroup) medicationIntroductionWebView.getParent()).removeView(medicationIntroductionWebView);
+            medicationIntroductionWebView.destroy();
+            medicationIntroductionWebView = null;
+        }
+        super.onDestroy();
+    }
 }
