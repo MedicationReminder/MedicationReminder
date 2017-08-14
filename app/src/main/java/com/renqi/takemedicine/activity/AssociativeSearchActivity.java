5eaff570365937c388377d7f8e1ceded527e62d8
Xu Wei <759974029@qq.com>
2017-08-14 21:40:21 +0800

    .....

--- app/src/main/java/com/renqi/takemedicine/activity/AssociativeSearchActivity.java
+++ app/src/main/java/com/renqi/takemedicine/activity/AssociativeSearchActivity.java
@@ -42,9 +42,29 @@ private RecyclerView medRecyList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         setToolBarTitle("请选择药名");
+         setToolBarTitle("请选择");
         medName.addTextChangedListener(watcher);
         medRecyList.setLayoutManager(new LinearLayoutManager(TakeMedicinApplication.getContext()));
+        commonAdapter =new CommonAdapter<String>(getApplicationContext(),R.layout.text_med_name_item,medNameList) {
+            @Override
+            public void convert(ViewHolder holder, final String s, int position) {
+                holder.setText(R.id.textMedName,s);
+
+                LinearLayout layout=holder.getView(R.id.medItemLayout);
+                layout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        medName.setText(s.trim());
+                        Intent intent=new Intent();
+                        intent.putExtra("medName",medName.getText().toString().trim());
+                        setResult(1,intent);
+                        finish();
+                    }
+                });
+            }
+        };
+        medRecyList.setAdapter(commonAdapter);
+        httpSearchLike();
     }
     private TextWatcher watcher = new TextWatcher() {
 
@@ -65,63 +85,49 @@ private RecyclerView medRecyList;
         @Override
         public void afterTextChanged(Editable s) {
             // TODO Auto-generated method stub
-            medNameList.clear();
-            RequestParams params=new RequestParams("http://101.69.181.251/api/v1/app_drugreminds/all_drugs_name");
-            params.addBodyParameter("name",medName.getText().toString().trim());
-            x.http().get(params, new Callback.CommonCallback<String>() {
-                @Override
-                public void onSuccess(String result) {
-                    try {
-                  JSONArray medNameJO= (new JSONObject(result)).getJSONArray("drug_names");
-                        if(medNameJO.length()==0) ToastUtils.showShortToast("没有相关"+medName.getText().toString().trim()+"的药品");
-                        for (int i = 0; i <medNameJO.length() ; i++) {
-                            JSONObject o=medNameJO.getJSONObject(i);
-                            medNameList.add(o.optString("name"));
-                        }
-
-                        commonAdapter =new CommonAdapter<String>(getApplicationContext(),R.layout.text_med_name_item,medNameList) {
-                            @Override
-                            public void convert(ViewHolder holder, final String s, int position) {
-                                holder.setText(R.id.textMedName,s);
-
-                                LinearLayout layout=holder.getView(R.id.medItemLayout);
-                                layout.setOnClickListener(new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View v) {
-                                        medName.setText(s.trim());
-                                        Intent intent=new Intent();
-                                        intent.putExtra("medName",medName.getText().toString().trim());
-                                        setResult(1,intent);
-                                        finish();
-                                    }
-                                });
-                            }
-
-
-                        };
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void onError(Throwable ex, boolean isOnCallback) {
-
-                }
-
-                @Override
-                public void onCancelled(CancelledException cex) {
-
-                }
-
-                @Override
-                public void onFinished() {
-               medRecyList.setAdapter(commonAdapter);
-                }
-            });
+             medNameList.clear();
+             httpSearchLike();
         }
     };
+
+    private void httpSearchLike() {
+        RequestParams params=new RequestParams("http://101.69.181.251/api/v1/app_drugreminds/all_drugs_name");
+        params.addBodyParameter("name",medName.getText().toString().trim());
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                try {
+                    JSONArray medNameJO= (new JSONObject(result)).getJSONArray("drug_names");
+                    if(medNameJO.length()==0) ToastUtils.showShortToast("没有相关"+medName.getText().toString().trim()+"的药品");
+                    for (int i = 0; i <medNameJO.length() ; i++) {
+                        JSONObject o=medNameJO.getJSONObject(i);
+                        medNameList.add(o.optString("name"));
+                    }
+                    commonAdapter.notifyDataSetChanged();
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
+    }
+
     @Event(R.id.home)
     private void home(View v)
     {
